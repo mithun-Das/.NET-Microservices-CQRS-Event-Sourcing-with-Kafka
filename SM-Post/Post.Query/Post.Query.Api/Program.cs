07164415e1ba7f87f49e1319614103d84f8c743f
mithun-Das <mithun.das227@gmail.com>
2023-04-27 16:45:38 +0600

    Section-12: Part-57 -> ConsumerHostedService

--- SM-Post/Post.Query/Post.Query.Api/Program.cs
+++ SM-Post/Post.Query/Post.Query.Api/Program.cs
@@ -29,6 +29,7 @@ builder.Services.AddScoped<IEventConsumer, EventConsumer>();
 
 
 builder.Services.AddControllers();
+builder.Services.AddHostedService<ConsumerHostedService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
