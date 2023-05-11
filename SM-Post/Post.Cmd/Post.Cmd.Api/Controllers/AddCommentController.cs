f5844c18eb23fd9ba85d5b435c18a1647f707546
mithun-Das <mithun.das227@gmail.com>
2023-05-11 23:13:27 +0600

    Section-13: Part-66 -> AddCommentController

---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Controllers/AddCommentController.cs
@@ -0,0 +1,65 @@
+﻿using CQRS.Core.Exceptions;
+using CQRS.Core.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Post.Cmd.Api.Commands;
+using Post.Common.DTOs;
+
+namespace Post.Cmd.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class AddCommentController : ControllerBase
+    {
+        private readonly ILogger<AddCommentController> _logger;
+        private readonly ICommandDispatcher _commandDispatcher;
+
+        public AddCommentController(ILogger<AddCommentController> logger, ICommandDispatcher commandDispatcher)
+        {
+            _logger = logger;
+            _commandDispatcher = commandDispatcher;
+        }
+
+        [HttpPut("{id}")]       // We kept it as HttpPut since we are adding resource to an existing resource
+        public async Task<ActionResult> AddCommentAsync(Guid id, AddCommentCommand command)
+        {
+            try
+            {
+                command.Id = id;
+                await _commandDispatcher.SendAsync(command);
+
+                return Ok(new BaseResponse
+                {
+                    Message = "Add Comment Request Completed Successfully!!"
+                });
+            }
+            catch (InvalidOperationException ex)
+            {
+                _logger.Log(LogLevel.Warning, ex, "Client made a bad request!");
+
+                return BadRequest(new BaseResponse
+                {
+                    Message = ex.Message,
+                });
+            }
+            catch (AggregateNotFoundException ex)
+            {
+                _logger.Log(LogLevel.Warning, ex, "Couldn't retrieve aggregate, client passed an incorrect post ID targeting the aggregate");
+
+                return BadRequest(new BaseResponse
+                {
+                    Message = ex.Message,
+                });
+            }
+            catch (Exception ex)
+            {
+                const string SAFE_ERROR_MESSAGE = "Error while processing request to add comment of post!";
+                _logger.Log(LogLevel.Error, ex, SAFE_ERROR_MESSAGE);
+
+                return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
+                {
+                    Message = SAFE_ERROR_MESSAGE
+                });
+            }
+        }
+    }
+}
