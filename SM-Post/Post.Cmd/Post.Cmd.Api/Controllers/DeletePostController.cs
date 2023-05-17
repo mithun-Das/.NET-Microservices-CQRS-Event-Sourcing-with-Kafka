6e4eb2865f53fe4161ad35397b319ad480b4a022
mithun-Das <mithun.das227@gmail.com>
2023-05-17 16:17:22 +0600

    Section-13: Part-72 -> DeletePostController

---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Controllers/DeletePostController.cs
@@ -0,0 +1,66 @@
+﻿using CQRS.Core.Exceptions;
+using CQRS.Core.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Post.Cmd.Api.Commands;
+using Post.Common.DTOs;
+
+namespace Post.Cmd.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DeletePostController : ControllerBase
+    {
+        private readonly ILogger<DeletePostController> _logger;
+        private readonly ICommandDispatcher _commandDispatcher;
+
+        public DeletePostController(ILogger<DeletePostController> logger, ICommandDispatcher commandDispatcher)
+        {
+            _logger = logger;
+            _commandDispatcher = commandDispatcher;
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeletePostAsync(Guid id, DeletePostCommand command)
+        {
+            try
+            {
+                command.Id = id;
+                await _commandDispatcher.SendAsync(command);
+
+                return Ok(new BaseResponse
+                {
+                    Message = "Delete Post Request Completed Successfully!!"
+                });
+            }
+            catch (InvalidOperationException ex)
+            {
+                _logger.Log(LogLevel.Warning, ex, "Client made a bad request!");
+
+                return BadRequest(new BaseResponse
+                {
+                    Message = ex.Message,
+                });
+            }
+            catch (AggregateNotFoundException ex)
+            {
+                _logger.Log(LogLevel.Warning, ex, "Couldn't retrieve aggregate, client passed an incorrect post ID targeting the aggregate");
+
+                return BadRequest(new BaseResponse
+                {
+                    Message = ex.Message,
+                });
+            }
+            catch (Exception ex)
+            {
+                const string SAFE_ERROR_MESSAGE = "Error while processing request to delete a post!";
+                _logger.Log(LogLevel.Error, ex, SAFE_ERROR_MESSAGE);
+
+                return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
+                {
+                    Message = SAFE_ERROR_MESSAGE
+                });
+            }
+        }
+    }
+}
